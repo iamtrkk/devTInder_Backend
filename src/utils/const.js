1c3c65e8d3a695b691383c160f847805e7b04ba6
iamtrkk <123228233+iamtrkk@users.noreply.github.com>
2025-04-12 00:55:15 +0530

    Verified check

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-const USER_SAFA_DATA = "firstName lastName photoUrl age gender about skills";
+const USER_SAFA_DATA = "firstName lastName photoUrl age gender about skills verified";
 
 module.exports = {
   USER_SAFA_DATA,
